2c5ca98733872d8b8aef08366c5e81be9fcefa52
nguyenpt <nguyenpt@bisync.jp>
2020-04-19 21:33:19 +0700

    [NguyenPT][issue_007] Using base layout for admin_actions

--- web/Modules/Admin/Resources/views/admin-actions/form.blade.php
+++ web/Modules/Admin/Resources/views/admin-actions/form.blade.php
@@ -1,89 +1,39 @@
 @php
 $theme = Config::get('app.theme');
 @endphp
-@extends($theme . '.layouts.master')
+@extends($theme . '.layouts.form')
 
-@section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h3 class="title-5 m-b-35">Edit</h3>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('admin-actions.index') }}"> Back</a>
-        </div>
+@section('form_content')
+<div class="row form-group">
+    <div class="col col-md-3">
+        {{ Form::label('name', null, ['class' => 'form-control-label']) }}
+    </div>
+    <div class="col-12 col-md-9">
+        {{ Form::text('name', $model->name, ['class' => 'form-control']) }}
     </div>
 </div>
-
-@if ($errors->any())
-<div class="alert alert-danger">
-    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <div class="pull-left">
-                    Edit Controller: {{ $model->name }}
-                </div>
-            </div>
-            @if (isset($model->id))
-            <form action="{{ route('admin-actions.update', $model->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-            @else
-            <form action="{{ route('admin-actions.store') }}" method="POST">
-                @csrf
-            @endif
-                <div class="card-body card-block">
-                        <div class="row form-group">
-                            <div class="col col-md-3">
-                                <label for="name" class=" form-control-label">Name</label>
-                            </div>
-                            <div class="col-12 col-md-9">
-                                <input type="text" name="name" placeholder="Name" class="form-control" value="{{ $model->name }}">
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="col col-md-3">
-                                <label for="key" class=" form-control-label">Key</label>
-                            </div>
-                            <div class="col-12 col-md-9">
-                                <input type="text" name="key" placeholder="Key" class="form-control" value="{{ $model->key }}">
-                            </div>
-                        </div>
-                         <div class="row form-group">
-                             <div class="col col-md-3">
-                                 <label for="controller_id" class=" form-control-label">Controller Id</label>
-                             </div>
-                             <div class="col-12 col-md-9">
-                                 <input type="text" name="controller_id" placeholder="Controller Id" class="form-control" value="{{ $model->controller_id }}">
-                             </div>
-                         </div>
-                         <div class="row form-group">
-                             <div class="col col-md-3">
-                                 <label for="permission" class=" form-control-label">Permission</label>
-                             </div>
-                             <div class="col-12 col-md-9">
-                                 <input type="text" name="permission" placeholder="Permission" class="form-control" value="{{ $model->permission }}">
-                             </div>
-                         </div>
-                </div>
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary btn-sm">
-                        <i class="fa fa-dot-circle-o"></i> Submit
-                    </button>
-                </div>
-            </form>
-        </div>
+<div class="row form-group">
+    <div class="col col-md-3">
+        {{ Form::label('key', null, ['class' => 'form-control-label']) }}
+    </div>
+    <div class="col-12 col-md-9">
+        {{ Form::text('key', $model->key, ['class' => 'form-control']) }}
     </div>
 </div>
-
+<div class="row form-group">
+    <div class="col col-md-3">
+        {{ Form::label('controller_id', null, ['class' => 'form-control-label']) }}
+    </div>
+    <div class="col-12 col-md-9">
+        {{ Form::text('controller_id', $model->controller_id, ['class' => 'form-control']) }}
+    </div>
+</div>
+<div class="row form-group">
+    <div class="col col-md-3">
+        {{ Form::label('permission', null, ['class' => 'form-control-label']) }}
+    </div>
+    <div class="col-12 col-md-9">
+        {{ Form::text('permission', $model->permission, ['class' => 'form-control']) }}
+    </div>
+</div>
 @endsection
--- web/Modules/Admin/Resources/views/admin-actions/show.blade.php
+++ web/Modules/Admin/Resources/views/admin-actions/show.blade.php
@@ -1,62 +1,42 @@
 @php
 $theme = Config::get('app.theme');
 @endphp
-@extends($theme . '.layouts.master')
+@extends($theme . '.layouts.show')
 
-@section('content')
-<div class="row">
-    <div class="col-md-12">
-        <div class="pull-left">
-            <h3 class="title-5 m-b-35">Show module</h3>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('admin-actions.index') }}"> Back</a>
-        </div>
+@section('header_content')
+Show action: {{ $model->name }}
+@endsection
+@section('data_content')
+<div class="row form-group">
+    <div class="col col-md-3">
+        <label class=" form-control-label">Name</label>
+    </div>
+    <div class="col-12 col-md-9">
+        <p class="form-control-static">{{ $model->name }}</p>
     </div>
 </div>
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <div class="pull-left">
-                    Show Module: {{ $model->name }}
-                </div>
-            </div>
-            <div class="card-body card-block">
-                <div class="row form-group">
-                    <div class="col col-md-3">
-                        <label class=" form-control-label">Name</label>
-                    </div>
-                    <div class="col-12 col-md-9">
-                        <p class="form-control-static">{{ $model->name }}</p>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col col-md-3">
-                        <label class=" form-control-label">Key</label>
-                    </div>
-                    <div class="col-12 col-md-9">
-                        <p class="form-control-static">{{ $model->key }}</p>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col col-md-3">
-                        <label class=" form-control-label">Controller Id</label>
-                    </div>
-                    <div class="col-12 col-md-9">
-                        <p class="form-control-static">{{ $model->controller_id }}</p>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col col-md-3">
-                        <label class=" form-control-label">Permission</label>
-                    </div>
-                    <div class="col-12 col-md-9">
-                        <p class="form-control-static">{{ $model->permission }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="row form-group">
+    <div class="col col-md-3">
+        <label class=" form-control-label">Key</label>
+    </div>
+    <div class="col-12 col-md-9">
+        <p class="form-control-static">{{ $model->key }}</p>
+    </div>
+</div>
+<div class="row form-group">
+    <div class="col col-md-3">
+        <label class=" form-control-label">Controller Id</label>
+    </div>
+    <div class="col-12 col-md-9">
+        <p class="form-control-static">{{ $model->controller_id }}</p>
+    </div>
+</div>
+<div class="row form-group">
+    <div class="col col-md-3">
+        <label class=" form-control-label">Permission</label>
+    </div>
+    <div class="col-12 col-md-9">
+        <p class="form-control-static">{{ $model->permission }}</p>
     </div>
 </div>
 @endsection
