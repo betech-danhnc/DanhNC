aa0f92b3d59e460683261422d23bd672c652cac3
betech-TrungPQ <61955033+betech-TrungPQ@users.noreply.github.com>
2020-04-23 17:16:15 +0700

    Update welcome.blade.php

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -5,34 +5,7 @@ $theme = Config::get('app.theme');
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <br>
-        <ul>
-            <li>
-                <a href="https://local-blab.betech-vn.com/admin">Admin</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/home">Home</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/login">Login</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/register">Register</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/admin/admin-modules">Modules</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/admin/admin-controllers">Controllers</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/admin/admin-actions">Actions</a>
-            </li>
-            <li>
-                <a href="https://local-blab.betech-vn.com/admin/admin-roles">Roles</a>
-            </li>
-        </ul>
-    </div>
+    
+
 </div>
 @endsection
