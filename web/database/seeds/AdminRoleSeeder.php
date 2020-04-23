ae9a2f58727e0714d35f68a961ecdada71fde858
betech-TrungPQ <61955033+betech-TrungPQ@users.noreply.github.com>
2020-04-23 17:13:19 +0700

    Update AdminRoleSeeder.php

--- web/database/seeds/AdminRoleSeeder.php
+++ web/database/seeds/AdminRoleSeeder.php
@@ -3,29 +3,27 @@
 use Illuminate\Database\Seeder;
 use Modules\Admin\Entities\AdminRole;
 
-class AdminRoleSeeder extends Seeder
-{
+class AdminRoleSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         AdminRole::truncate();
         $data = [
             [
-                'id'            => '1',
-                'name'          => 'Super admin',
-                'code'     => 'SUPER_ADMIN',
-                'working_type'   => '1',
-                'isStaff'   => '0',
-                'weight'   => '1',
-                'status'   => '1',
-                ]
-                
-           
+                'id' => '1',
+                'name' => 'Super admin',
+                'code' => 'SUPER_ADMIN',
+                'working_type' => '1',
+                'isStaff' => '0',
+                'weight' => '1',
+                'status' => '1',
+            ]
         ];
         AdminRole::insert($data);
     }
+
 }
