9545ca20ade82153ed6f50d61c2f262c27bc19ee
betech-TrungPQ <61955033+betech-TrungPQ@users.noreply.github.com>
2020-04-23 17:18:40 +0700

    Update AdminRole.php

--- web/Modules/Admin/Entities/AdminRole.php
+++ web/Modules/Admin/Entities/AdminRole.php
@@ -16,46 +16,46 @@ namespace Modules\Admin\Entities;
  * @property string $created_at     Created date
  * @property string $updated_at     Updated date
  */
-class Adm
-class AdminRole extends AdminModel
-{
+class AdminRole extends AdminModel {
     //-----------------------------------------------------
     // Constants
     //-----------------------------------------------------
     //-----------------------------------------------------
     // Properties
     //-----------------------------------------------------
+
     /** Fillable array */
     protected $fillable = [
-        'name', 'code', 'working_type', 'isStaff','weight', 'status', 'created_by'
+        'name', 'code', 'working_type', 'isStaff', 'weight', 'status', 'created_by'
     ];
-    
+
     //-----------------------------------------------------
     // Utility methods
     //----------------------------------------------------
+
     /**
      * {@inheritdoc}
      */
     public function getShowLink() {
         return url('admin/admin-roles', ['id' => $this->id]);
     }
-    
+
     //-----------------------------------------------------
     // Static methods
     //-----------------------------------------------------
+
     /**
      * {@inheritdoc}
      */
-    public static function getRules()
-    {
+    public static function getRules() {
         return [
-            'name'          => 'required',
-            'code'   => 'required',
-            'working_type' =>'required',
-            'isStaff'=>'required',
-            'weight'=>'required',
-            'status'=>'required',
+            'name' => 'required',
+            'code' => 'required',
+            'working_type' => 'required',
+            'isStaff' => 'required',
+            'weight' => 'required',
+            'status' => 'required',
         ];
     }
-}
 
+}
